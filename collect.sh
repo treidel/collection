f1029522c33de1d40dbc214d1ed5546856cbf695
Tim Reidel <reideltj@yahoo.com>
2014-08-03 04:19:52 +0000

    fixed timers

---
+++ collect.sh
@@ -0,0 +1,6 @@
+ROOT_DIR=/home/ubuntu/collection
+CERT_DIR=/home/ubuntu
+COLLECT_URL=https://54.86.219.87:8443/device/records
+DATABASE=/var/local/samples.db
+
+python $ROOT_DIR/application.py --url $COLLECT_URL --ca-cert $CERT_DIR/root.crt --device-cert $CERT_DIR/device.crt --device-key $CERT_DIR/device.key --database $DATABASE
